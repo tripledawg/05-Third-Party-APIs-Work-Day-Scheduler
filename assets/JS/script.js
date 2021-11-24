2c55dcbaac732c4809de9eadb8721fd15a947cbf
tripledawg <iamlesliecrouch>
2021-11-24 00:15:49 -0500

    created a list of timeblocks and started experimenting with 12 column layout and Bootstrap save button elements

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,4 @@
 //Today's date
 
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM do"));  //day spelled out, month spelled out, do NO YEAR
+$("#currentDay").text(today.format("dddd, MMMM do"));  //day spelled out, month spelled out, day of,  NO YEAR
