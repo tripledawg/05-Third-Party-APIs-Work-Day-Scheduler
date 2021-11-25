ac0775d02cc0005db5ffff56e04a78e1f736f49b
tripledawg <iamlesliecrouch>
2021-11-24 19:23:14 -0500

    connected time-block, row, and hour classes to html elements and added i tag with font awesome save icon

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,3 +2,27 @@
 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM do"));  //day spelled out, month spelled out, day of,  NO YEAR
+
+// var currentHour = parseInt(moment().format('H'));
+
+// var timeBlockEl = $('.time-block');
+// var descriptionEl = $('.description');
+// var rowEl = $('.row');
+// var hourEl = $('.hour'); 
+// var 
+
+// localStorage.setItem("eventToSave", "value"); 
+
+
+
+// if hour = hour 
+
+// El.attr('class', 'present') OR  El.addClass('present');
+
+// hour = past  
+
+// El.attr('class', 'past') OR El.addClass('past');
+
+// else 
+
+// El.attr ('class', 'future') OR  El.addClass('future');
