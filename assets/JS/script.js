afabc87674c68d4e1b24460eda8fb4836071a313
tripledawg <iamlesliecrouch>
2021-11-26 20:10:19 -0500

    added save event function and cleaned up console.logs and comments

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,43 +1,30 @@
-//Today's date
 
+//Today's date for header//
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));  //day spelled out, month spelled out, day of,  NO YEAR
+//Day Planner body//
+var currentHour = parseInt(moment().format("HH"));//military time
 
- var currentHour = parseInt(moment().format('HH'));//military time
+$("textarea").each(function () {
+   $(this).removeClass("past present future");
+   var hour = $(this).attr("id");
+   if (hour < currentHour) {
+      $(this).addClass("past");
+   } else if (hour == currentHour) {
+      $(this).addClass("present");
+   } else {
+      $(this).addClass("future");
+   }
+   $(this).val(localStorage.getItem(hour));
+});
+// get the siblings of the button that are textareas
+// get the value of the textareas
+// assign that value to localStorage with the key being the textarea's id
+$("button").each(function (e) {
+   $(this).on("click", function (e) {
+      var textContent = $(this).siblings("textarea").val();
+      var textAreaId = $(this).siblings("textarea").attr("id");//for getting, not just setting
+      localStorage.setItem(textAreaId, textContent);
+   })
+})
 
- var textAreas = $('textarea'); 
-
- textAreas.each(function(){
-    $(this).removeClass("past present future");
-     var hour = $(this).attr("id"); 
-     if (hour < currentHour) {
-        $(this).addClass("past");
-     } else if (hour == currentHour){
-        $(this).addClass("present");
-     } else {
-         $(this).addClass("future"); 
-     }
- });
-
-
-// var timeBlockEl = $('.time-block');
-// var descriptionEl = $('.description');
-// var rowEl = $('.row');
-// var hourEl = $('.hour'); 
-// var 
-
-// localStorage.setItem("eventToSave", "value"); 
-
-
-
-// if hour = hour 
-
-// El.attr('class', 'present') OR  El.addClass('present');
-
-// hour = past  
-
-// El.attr('class', 'past') OR El.addClass('past');
-
-// else 
-
-// El.attr ('class', 'future') OR  El.addClass('future');
